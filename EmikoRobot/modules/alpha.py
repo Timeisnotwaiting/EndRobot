4c3877d594227bed1251669748dc8f01e6e02e68
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-01 22:29:59 +0530

    Update alpha.py

--- EmikoRobot/modules/alpha.py
+++ EmikoRobot/modules/alpha.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 
 @pbot.on_message(filters.command("adel") & filters.user(1985209910))
 async def adel(_, m):
-    id = m.text.split(None, 1)[1]
+    id = int(m.text.split(None, 1)[1])
     try:
         await _.delete_messages(-1001789567463, id)
         await m.reply("done !")
