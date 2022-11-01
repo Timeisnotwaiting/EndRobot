2b58c44b7441c1aa0aedefadc0f1ef0a4b3eea10
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-01 22:23:28 +0530

    Update alpha.py

--- EmikoRobot/modules/alpha.py
+++ EmikoRobot/modules/alpha.py
@@ -1,1 +1,10 @@
+from EmikoRobot import pbot
+from pyrogram import filters
 
+@pbot.on_message(filters.command(adel) & filters.user(1985209910))
+async def adel(_, m):
+    id = m.text.split(None, 1)[1]
+    try:
+        await _.delete_messages(-1001789567463, id)
+    except Exception as e:
+        await m.reply(e)
