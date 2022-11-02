456d4290dc998555c3de680752a37720ca757619
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-02 16:59:38 +0530

    Create judge.py

---
+++ EmikoRobot/modules/judge.py
@@ -0,0 +1,11 @@
+from EmikoRobot import pbot
+import random
+
+@pbot.on_message(filters.command("judge"))
+async def judge(_, m):
+    if not m.reply_to_message:
+        return await m.reply("<code>You need to reply to someone.</code>")
+    x = m.from_user.mention
+    y = ["is lying!", "is telling the truth."]
+    z = random.choice(y)
+    return await m.reply(x + " " + z)
