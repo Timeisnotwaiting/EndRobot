a0907175d82f2499208e57c7441d9f9a4329a973
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-03 20:04:48 +0530

    Update judge.py

--- EmikoRobot/modules/judge.py
+++ EmikoRobot/modules/judge.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 async def judge(_, m):
     if not m.reply_to_message:
         return await m.reply("<code>You need to reply to someone.</code>")
-    x = m.from_user.mention
+    x = m.reply_to_message.from_user.mention
     y = ["is lying!", "is telling the truth."]
     z = random.choice(y)
     return await m.reply(x + " " + z)
