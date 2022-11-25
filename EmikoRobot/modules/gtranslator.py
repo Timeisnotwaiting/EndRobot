d75d1f2f9f37092ede330f2ace5a91eea96a6431
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-25 08:15:26 +0530

    Update gtranslator.py

--- EmikoRobot/modules/gtranslator.py
+++ EmikoRobot/modules/gtranslator.py
@@ -69,7 +69,7 @@ async def translate(_, message: Message) -> None:
         f"<code>{translation.text}</code>"
     )
 
-    await message.reply_text(reply, parse_mode="html")
+    await message.reply_text(reply)
 
 
 def languages(update: Update, context: CallbackContext) -> None:
