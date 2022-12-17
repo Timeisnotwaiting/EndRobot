9de1f3712a41e217de4afeba72a6d0ee15ab28d6
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-17 11:25:00 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -7,6 +7,7 @@ from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
 from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot, BOT_USERNAME as bu
+from config import BOT_NAME
 
 
 PHOTO = "https://te.legra.ph/file/345715f7a17d1eadf5307.jpg"
@@ -15,9 +16,9 @@ PHOTO = "https://te.legra.ph/file/345715f7a17d1eadf5307.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Killua.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [:THE END:](https://t.me/THE_END_NETWORK)** \n\n"
+  TEXT += f"⚪ **My Master : [SPL](https://t.me/SpLBots)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  BUTTON = [[Button.url("Help", f"https://t.me/{bu}?start=help"), Button.url("Support", "https://t.me/THE_END_NETWORK")]]
+  BUTTON = [[Button.url("Help", f"https://t.me/{bu}?start=help"), Button.url("Support", "https://t.me/Coding_Bots")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
